8bb4dc04c540d4a58c0832e91a657f60800873ca
Fofiko <foteinid@gmail>
2018-08-10 17:07:58 +0100

    add song

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,9 +1,9 @@
 require("minitest/autorun")
 require("minitest/rg")
 require("pry")
-require_relative("../guest.rb")
 require_relative("../song.rb")
-# require_relative("../ccc.rb")
+require_relative("../guest.rb")
+require_relative("../room.rb")
 
 class RoomTest < MiniTest::Test
 
--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -1,15 +1,19 @@
 require("minitest/autorun")
 require("minitest/rg")
 require("pry")
-require_relative("../room.rb")
+require_relative("../song.rb")
 require_relative("../guest.rb")
-# require_relative("../ccc.rb")
+require_relative("../room.rb")
 
 class SongTest < MiniTest::Test
 
+  def setup
+    @song1 = Song.new("rare hearts")
+  end
 
-
-
+  def test_song_has_title
+    assert_equal("rare hearts", @song1.title)
+  end
 
 
 
